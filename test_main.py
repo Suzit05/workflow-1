2a4d1ad169ee707dd37feb42b155992b7cd37ab9
Suzit05 <103447157+Suzit05@users.noreply.github.com>
2025-07-23 14:53:07 +0530

    CI modified 2

--- test_main.py
+++ test_main.py
@@ -4,5 +4,5 @@ from main import add
 
 class TestMain(unittest.TestCase):
     def test_add(self):
-        assert(2,3) == 5
-        assert(-1,1) == 0
+        assert add(2,3) == 5
+        assert add(-1,1) == 0
